e5955c2a18b69ff96ed9dc94c4fa3d3833980863
Dominic Petriella <dompetriella@gmail.com>
2024-10-27 13:03:06 -0400

    added state for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
+import { act } from "react";
 import "./App.css";
 import Card from "./components/card";
-import { CardColor, CardData } from "./models/cardData";
+import { CardData } from "./models/cardData";
+import useCardGameStore from "./state/appStore";
 
 function App() {
+
+  const playerHand = useCardGameStore((state) => state.playerHand);
+  const activeCard = useCardGameStore((state) => state.currentActiveCard);
+  const opponentHand = useCardGameStore((state) => state.opponentHand);
+
   return (
     <>
       <div className="flex flex-col w-screen h-screen">
-        <div className="flex justify-center items-center h-1/2 bg-green-400 size-full">
-          <Card cardData={new CardData(CardColor.Green, 10)} isSmall={true} />
-          <Card cardData={new CardData(CardColor.Green, 10)} isSmall={true} />
-          <Card cardData={new CardData(CardColor.Green, 10)} isSmall={true} />
+        <div className="flex justify-center items-center  h-1/2  bg-green-400 size-full">
+          {opponentHand.map((card: CardData) => (
+            <Card cardData={card} isSmall={true} />
+          ))}
+
         </div>
-        <div className="flex justify-center items-center bg-blue-400 size-full">
-          <Card cardData={new CardData(CardColor.Blue, 10)} isFaceUp={true} />
+        <div className="flex justify-center items-center    bg-blue-400 size-full">
+          <Card cardData={activeCard!} isFaceUp={true} />
         </div>
-        <div className="flex bg-red-400 w-screen justify-center p-4">
-          <Card
-            cardData={new CardData(CardColor.Blue, 10)}
-            isFaceUp={true}
-            isPlayerOwned={true}
-          />
-          <Card
-            cardData={new CardData(CardColor.Red, 10)}
-            isFaceUp={true}
-            isPlayerOwned={true}
-          />
-          <Card
-            cardData={new CardData(CardColor.Green, 10)}
-            isFaceUp={true}
-            isPlayerOwned={true}
-          />
-          <Card
-            cardData={new CardData(CardColor.Yellow, 10)}
-            isFaceUp={true}
-            isPlayerOwned={true}
-          />
+        <div className="flex bg-red-400 w-screen h-1/3 justify-center p-4">
+          {playerHand.map((card: CardData) => (
+            <Card cardData={card} isFaceUp={true} isPlayerOwned={true} />
+          ))}
+
         </div>
       </div>
     </>
